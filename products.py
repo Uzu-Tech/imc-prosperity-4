495b61237c9939cfab823a15df64bde274eb1e65
Kobi Uzu <kobiuzu3@gmail.com>
2026-04-29 09:28:08 +0100

    Add products

---
+++ products.py
@@ -0,0 +1,72 @@
+PRODUCTS = {
+    "Galaxy Sounds Recorders": [
+        "GALAXY_SOUNDS_DARK_MATTER",
+        "GALAXY_SOUNDS_BLACK_HOLES",
+        "GALAXY_SOUNDS_PLANETARY_RINGS",
+        "GALAXY_SOUNDS_SOLAR_WINDS",
+        "GALAXY_SOUNDS_SOLAR_FLAMES"
+    ],
+    "Vertical Sleeping Pods": [
+        "SLEEP_POD_SUEDE",
+        "SLEEP_POD_LAMB_WOOL",
+        "SLEEP_POD_POLYESTER",
+        "SLEEP_POD_NYLON",
+        "SLEEP_POD_COTTON"
+    ],
+    "Organic Microchips": [
+        "MICROCHIP_CIRCLE",
+        "MICROCHIP_OVAL",
+        "MICROCHIP_SQUARE",
+        "MICROCHIP_RECTANGLE",
+        "MICROCHIP_TRIANGLE"
+    ],
+    "Purification Pebbles": [
+        "PEBBLES_XS",
+        "PEBBLES_S",
+        "PEBBLES_M",
+        "PEBBLES_L",
+        "PEBBLES_XL"
+    ],
+    "Domestic Robots": [
+        "ROBOT_VACUUMING",
+        "ROBOT_MOPPING",
+        "ROBOT_DISHES",
+        "ROBOT_LAUNDRY",
+        "ROBOT_IRONING"
+    ],
+    "UV-Visors": [
+        "UV_VISOR_YELLOW",
+        "UV_VISOR_AMBER",
+        "UV_VISOR_ORANGE",
+        "UV_VISOR_RED",
+        "UV_VISOR_MAGENTA"
+    ],
+    "Instant Translators": [
+        "TRANSLATOR_SPACE_GRAY",
+        "TRANSLATOR_ASTRO_BLACK",
+        "TRANSLATOR_ECLIPSE_CHARCOAL",
+        "TRANSLATOR_GRAPHITE_MIST",
+        "TRANSLATOR_VOID_BLUE"
+    ],
+    "Construction Panels": [
+        "PANEL_1X2",
+        "PANEL_2X2",
+        "PANEL_1X4",
+        "PANEL_2X4",
+        "PANEL_4X4"
+    ],
+    "Liquid Breath Oxygen Shakes": [
+        "OXYGEN_SHAKE_MORNING_BREATH",
+        "OXYGEN_SHAKE_EVENING_BREATH",
+        "OXYGEN_SHAKE_MINT",
+        "OXYGEN_SHAKE_CHOCOLATE",
+        "OXYGEN_SHAKE_GARLIC"
+    ],
+    "Protein Snack Packs": [
+        "SNACKPACK_CHOCOLATE",
+        "SNACKPACK_VANILLA",
+        "SNACKPACK_PISTACHIO",
+        "SNACKPACK_STRAWBERRY",
+        "SNACKPACK_RASPBERRY"
+    ]
+}
